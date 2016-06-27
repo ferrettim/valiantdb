8d7b8c62cd2dac4da56ea643df6c94a53170626b
Martin <ferrettimartin@gmail.com>
2016-06-27 10:47:14 -0400

    Minor update

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,10 +5,10 @@ class RegistrationsController < Devise::RegistrationsController
   private
  
   def sign_up_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, :avatar, :tableview, :voice, :level_id, :score, :pubfourfiveone, :pubaftershock, :pubaspen, :pubavatar, :pubblackmask, :pubboom, :pubdarkhorse, :pubdc, :pubdynamite, :pubidw, :pubimage, :pubmarvel, :pubvaliant, :pubvertigo, :pubzenescope)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :avatar, :tableview, :voice, :level_id, :score, :pubfourfiveone, :pubaftershock, :pubarchie, :pubaspen, :pubavatar, :pubblackmask, :pubboom, :pubdarkhorse, :pubdc, :pubdynamite, :pubidw, :pubimage, :pubmarvel, :pubvaliant, :pubvertigo, :pubzenescope)
   end
  
   def account_update_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password, :avatar, :tableview, :voice, :level_id, :score, :pubfourfiveone, :pubaftershock, :pubaspen, :pubavatar, :pubblackmask, :pubboom, :pubdarkhorse, :pubdc, :pubdynamite, :pubidw, :pubimage, :pubmarvel, :pubvaliant, :pubvertigo, :pubzenescope)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password, :avatar, :tableview, :voice, :level_id, :score, :pubfourfiveone, :pubaftershock, :pubarchie, :pubaspen, :pubavatar, :pubblackmask, :pubboom, :pubdarkhorse, :pubdc, :pubdynamite, :pubidw, :pubimage, :pubmarvel, :pubvaliant, :pubvertigo, :pubzenescope)
   end
 end  
