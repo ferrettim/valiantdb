866eee118947638f8f089633de1a33e2ecb24468
Martin <ferrettimartin@gmail.com>
2016-06-30 19:52:35 -0400

    Minor update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,8 @@ Rails.application.configure do
   authentication: "plain",
   enable_starttls_auto: true,
   user_name: ENV["CA_USERNAME"],
-  password: ENV["CA_PASSWORD"]
+  password: ENV["CA_PASSWORD"],
+  openssl_verify_mode: "none"
   }
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
